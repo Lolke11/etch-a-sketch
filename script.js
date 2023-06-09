37e5c0f0edbba2d7188401a4e3d7b9558c2585a1
Lolke11 <nenad.skokovic@gmail.com>
2023-06-09 11:36:12 +0200

    Made grid in js

---
+++ script.js
@@ -0,0 +1,16 @@
+let container = document.getElementsByClassName("container");
+
+let size = 16;
+for (let i = 0; i < size; i++) {
+    let row = document.createElement("div");
+    row.className = "row";
+    for (let j = 1; j < size; j++) {
+        let cell = document.createElement("div");
+        cell.className = "gridsquare";
+        cell.textContent = (i * size) + j;
+        row.appendChild(cell);
+    }
+    container.appendChild(row);
+}
+
+container.appendChild(div);
