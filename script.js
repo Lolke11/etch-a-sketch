409577dde818bfde3497a32f75c0a2a01bd6ace8
Lolke11 <nenad.skokovic@gmail.com>
2023-06-10 20:30:24 +0200

    Css started

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let container = document.querySelector(".container");
 
-let size = 16;
+let size = 8;
 for (let i = 0; i < size; i++) {
-    let row = document.createElement("div");
-    row.className = "row";
-    for (let j = 1; j < size; j++) {
-        let cell = document.createElement("div");
-        cell.className = "gridsquare";
-        cell.textContent = (i * size) + j;
-        row.appendChild(cell);
+    let column = document.createElement("div");
+    column.className = "column";
+    for (let j = 1; j <= size; j++) {
+        let row = document.createElement("div");
+        row.className = "row";
+        row.textContent = (i * size) + j;
+        column.appendChild(row);
     }
-    container.appendChild(row);
+    container.appendChild(column);
 }
