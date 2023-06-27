245a0dd5be3c56653af69059786a4b98c233efdb
Lolke11 <nenad.skokovic@gmail.com>
2023-06-27 09:14:11 +0200

    Painting works

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 let container = document.querySelector(".container");
 
-let size = 8;
-for (let i = 0; i < size; i++) {
-    let column = document.createElement("div");
-    column.className = "column";
-    for (let j = 1; j <= size; j++) {
-        let row = document.createElement("div");
-        row.className = "row";
-        column.appendChild(row);
-    }
-    container.appendChild(column);
-}
-
 let color = "#000000";
 function ChangeColor() {
     color = document.querySelector("#colorpicker").value;
 }
 
-let rows = document.querySelectorAll(".row");
-for (let i = 0; i < rows.length; i++) {
-    rows[i].addEventListener("mouseover", function() {
-        rows[i].style.backgroundColor = color;
-    })
-}
+container.addEventListener("mouseover", function () {
+    let rows = document.querySelectorAll(".row");
+    for (let i = 0; i < rows.length; i++) {
+        rows[i].addEventListener("mouseover", function() {
+            rows[i].style.backgroundColor = color;
+        })
+    }
+})
+
+let gridSize = document.querySelector("#sizeValue");
+let pickedSize = document.querySelector("#sizeSlider");
+
+pickedSize.addEventListener("change", function () {
+    let s = pickedSize.value;
+    gridSize.textContent = s + "x" + s;
+
+    let size = s;
+    for (let i = 0; i < size; i++) {
+        let column = document.createElement("div");
+        column.className = "column";
+        for (let j = 1; j <= size; j++) {
+            let row = document.createElement("div");
+            row.className = "row";
+            column.appendChild(row);
+        }
+        container.appendChild(column);
+    }
+})
